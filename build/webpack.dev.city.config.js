7f05f08039fe7eaf0edc399790e3f9066d2ecd7b
haitao <zltwht@vip.qq.com>
2018-09-30 11:29:14 +0800

    修改测试地址

--- build/webpack.dev.city.config.js
+++ build/webpack.dev.city.config.js
@@ -71,7 +71,7 @@ module.exports = merge(webpackBaseConfig, {
             //匹配代理的url
             '/cmsapi': {
                 // 目标服务器地址
-                target: 'http://192.168.1.42:8083/',
+                target: 'http://city.test.toutiaofangchan.com/',
                 //路径重写
                 pathRewrite: {'^/cmsapi' : '/cmsapi'},
                 changeOrigin: true
