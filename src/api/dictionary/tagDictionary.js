40892429473cab3a5b0e9be51c98432c794432a5
ChyTesT <18710160187@163.com>
2018-10-10 18:36:52 +0800

    栏目页

--- src/api/dictionary/tagDictionary.js
+++ src/api/dictionary/tagDictionary.js
@@ -2,12 +2,12 @@ import http from '../api.base';
 export default {
     //标签列表接口  /newsTagDictionary/newsTagsList
     getTagDictionaryList(params) {
-        return http.post('/newsTagDictionary/newsTagsList',params);
+        return http.post('cmsapi/newsTagDictionary/newsTagsList',params);
     },
     updateTagById(params){
-        return http.post('/newsTagDictionary/updateNEwsTag',params);
+        return http.post('cmsapi/newsTagDictionary/updateNEwsTag',params);
     },
     addNewsTagApi(params){
-        return http.post('/newsTagDictionary/addNewsTag',params);
+        return http.post('cmsapi/newsTagDictionary/addNewsTag',params);
     },
 }
