b28681845c0f6128267ed21c7846329d79df141b
haitao <zltwht@vip.qq.com>
2018-11-05 15:13:12 +0800

    自助建站布局

--- src/views/wysiwyg/main.vue
+++ src/views/wysiwyg/main.vue
@@ -30,11 +30,184 @@
         -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=100)";
         filter: alpha(opacity=100);
     }
+    .wys-main {
+        width: 1920px;
+        height: 100%;
+        background: #FFFFFF;
+    }
+    .wys-menu-left {
+        width: 320px;
+        overflow: hidden;
+        float: left;
+        box-sizing: border-box;
 
+    }
+    .wys-content {
+        width: 1200px;
+        float: left;
+        box-sizing: border-box;
+        position: relative;
+        max-height: 100%;
+        height: 100%;
+    }
+    .wys-isscrll {
+        padding: 0px 360px 0 360px;
+        overflow: auto;
+        background: #F2F3F4;
+        max-height: 100%;
+        height: 100%;
+    }
+    .wys-contentOver {
+        width: 375px;
+        text-align: center;
+        background: #FFFFFF;
+        overflow: hidden;
+    }
+    /*滚动条样式*/
+    .wys-isscrll::-webkit-scrollbar {/*滚动条整体样式*/
+        width: 4px;     /*高宽分别对应横竖滚动条的尺寸*/
+        height: 4px;
+    }
+    .wys-isscrll::-webkit-scrollbar-thumb {/*滚动条里面小方块*/
+        border-radius: 5px;
+        -webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.2);
+        background: #CACBCB;
+    }
+    .wys-isscrll::-webkit-scrollbar-track {/*滚动条里面轨道*/
+        -webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.2);
+        border-radius: 0;
+        background: #EDEEEF;
+    }
+    .wys_title {
+        position: absolute;
+        top: -40px;
+        left: 0;
+        width: 160px;
+        text-align: center;
+        line-height: 40px;
+        border-radius: 10px 10px 0 0;
+        height: 40px;
+        background: #f2f3f4;
+        z-index: 10;
+    }
 </style>
 
 <template>
-    <div class="main" >
+
+ <div class="wys-main" >
+     <div class="wys-header">
+         <div class="wys-header-left"><img src="http://wap-qn.toutiaofangchan.com/adideas/4fa0cb767c5a42e7af2aa85003704eb1.jpg"/> </div>
+         <div class="wys-header-content"></div>
+     </div>
+     <div class="wys-menu-left">建站模板列表</div>
+     <div class="wys-content">
+        <div class="wys_title"><Icon type="ios-home" style="margin-right:5px;"></Icon>站点编辑器</div>
+         <div class="wys-isscrll">
+            <div class="wys-contentOver"><!-- 自助建站网页html容器 -->
+                <ul>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                    <li>fdsfdsfdsfdsf</li>
+                </ul>
+            </div>
+         </div>
+     </div>
+     <div class="wys-edit-right">建站编辑器</div>
+ </div>
+    <!-- <div class="main" >
         <div class="sidebar-menu-con" :style="{width: '200px', overflow:  'auto'}">
 
             <div id="wysiwyg_componentbox" wys-container>
@@ -53,7 +226,7 @@
                 <component v-bind:is="currentEditor" :key="currentEditorKey"></component>
             </navigation>
         </div>
-    </div>
+    </div> -->
 </template>
 <script>
     import navigation from '@/libs/navigation';
