659621b03263c853d912bf036c89d9fa06378260
Administrator <admin@example.com>
2018-11-15 20:51:46 +0800

    Merge branch 'dev' of 39.105.31.58:front/Art-cms into dev
    # Conflicts:
    #	src/views/wysiwyg/wys-conf/component-stage.js

--- src/libs/stage-javascript-include-loader.js
+++ src/libs/stage-javascript-include-loader.js
@@ -1,4 +1,4 @@
-const _ = require.resolve('lodash')
+const _ = require('lodash')
 module.exports = function (source, map) {
     this.callback(null, 'module.exports = function(Component) {Component.options.wys_stageJavascript_import = Component.options.wys_stageJavascript_import || [];' +
         'Component.options.wys_stageJavascript_import.push(' +
