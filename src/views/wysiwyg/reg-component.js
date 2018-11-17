de93e16be5830e50ccafc21bdbd8923a084339d2
haitao <zltwht@vip.qq.com>
2018-11-17 17:40:37 +0800

    tab组件

--- src/views/wysiwyg/reg-component.js
+++ src/views/wysiwyg/reg-component.js
@@ -7,6 +7,7 @@ import wys_navigation from './wys-view/simple/wys-navigation.vue';
 import splitcharacter from './wys-view/simple/wys-splitcharacter.vue';
 import wys_link from './wys-view/simple/wys-link.vue';
 import wys_placeholder from './wys-view/simple/wys-placeholder.vue';
+import wys_tab from './wys-view/simple/wys-tab.vue';
 
 export default {
 
@@ -65,6 +66,12 @@ export default {
             title:'链接',
             component: wys_link,
             id: 'wys_link'
+        },
+        {
+            icon: 'http://wap-qn.toutiaofangchan.com/adideas/fe3142c7596d47789df1c2b84d3d88d6.png',
+            title:'Tab',
+            component: wys_tab,
+            id: 'wys_tab'
         },
     ]
 };
--- src/views/wysiwyg/wys-view/simple/wys-tab.vue
+++ src/views/wysiwyg/wys-view/simple/wys-tab.vue
@@ -1,137 +1,241 @@
 <template>
-    <Form  :label-width="60" class="imgWidthCont">
-
-
-                <FormItem label="列数" >
-                    <Select v-model="share.count" @on-change="countchange">
-                        <Option  :value="2" :key="2">2</Option>
-                        <Option  :value="3" :key="3">3</Option>
-                        <Option  :value="4" :key="4">4</Option>
-                        <Option  :value="5" :key="5">5</Option>
-                    </Select>
-                </FormItem>
-
-
-
-
-
+       <Form  :label-width="60" class="imgWidthCont">
+    <Tabs>  
+        <TabPane label="内容" >
+            <div v-for="item,index in share.tabList" v-dragging="{ item: item, list: share.tabList, group: 'item' }" @click="navClick(item)">
+                <Row class="navdhFont">
+                    <Col span="12">Tab{{index+1}}</Col>
+                    <Col span="12" style="text-align:right;"><Icon @click.native="removeNavigat(item)" type="trash-a"></Icon></Col>
+                </Row>
+                <Row class="navdhName"> 
+                    <Col span="24">
+                        <FormItem  label="名称">
+                            <Input v-model="item.name"></Input>
+                        </FormItem>  
+                    </Col>
+                </Row>
+            </div>
+            <Button style="width:280px; margin-top:20px;" @click="addNaviPush()"><Icon type="plus-round" style="margin-right:10px;"></Icon>点击添加</Button>
+        </TabPane>
+    </Tabs>
     </Form>
-
-
-
 </template>
 
 <script>
 
     export default {
-        name: 'wys-placeholder',
+        name: 'wys-img',
         data() {
             return {
                 share:{
-                    count:2,
-                    ids:[],
+                    imgTop:0,
+                    imgRight:0,
+                    imgBottom:0,
+                    imgLeft:0,
+                    navVertical:"navitem0",
+                    tabCount:2,
                     increase:1,
-                    width:50,
-
-                }
+                    tabListDomid:"",
+                    tabDomid:"",
+                    isDel:false,
+                    tabList:[
+                        // {name:"Tab1",url:"",navVisible:false},
+                        // {name:"Tab2",url:"",navVisible:false},
+                    ]
+                },
             };
         },
         methods: {
-            countchange:function (val) {
-
-
-                if(this.share.ids.length>val){
-                    while (this.share.ids.length - val >0){
-                        console.log(this.share.ids)
-                        var id=this.share.ids.pop();
-                        $('#'+id).remove();
-                    }
+            removeNavigat(item){
+                if($("#"+item.id).attr("class").indexOf("cur") > 0){
+                     this.$Message.error('当前选中对象不能删除');
+                     return false;
                 }
-                else if(this.share.ids.length<val){
-                    while (val - this.share.ids.length>0){
-                        this.share.increase += 1;
-                        this.share.ids.push(this.$vnode.key+'-'+this.share.increase);
-                    }
-                }
-                if(this.share.count>0){
-                    this.share.width = parseInt(100/this.share.count);
-                }
-
+                $("#"+item.id).remove();
+                $("#"+item.cid).remove();
+                let index = this.share.tabList.indexOf(item);
+                this.share.tabList.splice(index,1);
+                this.share.isDel = true;
+            },
+            addUrl(item){
+                item.navVisible = true;
+            },
+            addNaviPush(){
+                this.share.isDel = true;
+                this.share.increase += 1;
+                this.share.tabCount += 1;
+                this.share.tabList.push({id:this.$vnode.key+'-'+this.share.increase,cid:'con-'+this.$vnode.key+'-'+this.share.increase,name:"新导航",url:"",navVisible:false});
+            },
+            navClick(item){
+                this.share.tabDomid = item.id;
+                this.share.tabListDomid = item.cid;
+            },
+            tabCreatpush:function () {
+               for (let index = 0; index < this.share.tabCount; index++) {
+                    this.share.increase += 1;
+                    this.share.tabList.push({id:this.$vnode.key+'-'+this.share.increase,cid:'con-'+this.$vnode.key+'-'+this.share.increase,name:"Tab"+index,url:"",navVisible:false});
+               }
+               this.share.tabDomid = this.share.tabList[0].id;
+               this.share.tabListDomid = this.share.tabList[0].cid;
+               this.share.isDel = false;
             }
         },
         created: function () {
-            this.countchange(this.share.count);
+            this.tabCreatpush();
+            // console.log('created',this.$options.customOption,this.$options.wysdocs,this.$options) // => 'foo'
         },
-        mounted () {
+        mounted() {
+            this.$dragging.$on('dragged', ({ value }) => {
+                console.log(value.item)
+                console.log(value.list)
+                console.log(value.group)
+            });
         }
     };
 </script>
 
 <style scoped>
+.navdhFont {
+    font-size: 20px;
+    border-top: 1px solid #cccccc;
+    margin-right: 20px;
+    padding-top: 10px;
+}
+.navdhName {
+    margin-top: 20px;
+    padding-right: 20px;
+}
 </style>
 
 
 <stage-template>
 </stage-template>
-<stage-js-wys>
-    if($t.children('ul').length<1){
-        $t.append('<ul class="layout-container-table edit-page"></ul>');
-    }
 
-    <% for(var i = 0; i < share.ids.length; i++){ %>
-    if($('#<%= share.ids[i] %>').length<1){
-        <% if (i==0) { %>
-            $('<li style="width:<%= share.width %>%" id="<%= share.ids[i] %>" class="snapshoot-box" id="<%= share.ids[i] %>" wys-container ></li>').prependTo($t.children('ul'));
-        <% } %>
-        <% if (i!=0) { %>
-            $('<li style="width:<%= share.width %>%" id="<%= share.ids[i] %>" class="snapshoot-box" id="<%= share.ids[i] %>" wys-container ></li>').insertAfter($t.children('ul').children('li').eq(<%= i-1 %>));
-        <% } %>
+<stage-js-wys>
+    if(!<%= share.isDel %>){
+        $t.children(".wys-nav-List").children(".nav-inner").find(".nav-item").removeClass("cur");
+        $t.children(".tabDivList").find(".tabContainer").hide();
+    }
+    for(var i=0; i<$t.children(".wys-nav-List").children(".nav-inner").find(".nav-item").length; i++){
+        if($($t.children(".wys-nav-List").children(".nav-inner").find(".nav-item")[i]).attr("id") == "<%= share.tabDomid %>"){
+            $($t.children(".wys-nav-List").children(".nav-inner").find(".nav-item")[i]).addClass("cur");
+        }
+    }
+    for(var i=0; i<$t.children(".tabDivList").find(".tabContainer").length; i++){
+        if($($t.children(".tabDivList").find("div")[i]).attr("id") == "<%= share.tabListDomid %>"){
+            $($t.children(".tabDivList").find("div")[i]).show();
+        }
+    }
+    for(var i=0; i<$t.children(".tabDivList").find("div").length; i++){
+        if($($t.children(".tabDivList").find("div")[i]).attr("id") == "<%= share.tabListDomid %>"){
+            $($t.children(".tabDivList").find("div")[i]).show();
+        }
+    }
+    if($t.children('div').length<2){
+        $t.append('<div class="wys-nav-List navitem1"><ul class="nav-inner"></ul></div>');
+        $t.append('<div class="tabDivList"></div>');
+    }
+    <% for(var i = 0; i < share.tabList.length; i++){ %>
+    if($('#<%= share.tabList[i].id %>').length<1){
+            <% if (i==0) { %>
+                $('<li id="<%= share.tabList[i].id %>" class="cur nav-item"><%= share.tabList[i].name %></li>').prependTo($t.children().children('ul'));        
+                $('<div id="<%= share.tabList[i].cid %>" class="tabContainer" wys-container></div>').prependTo($t.children(".tabDivList"));
+            <% } %>
+            <% if (i!=0) { %>
+                 $('<li id="<%= share.tabList[i].id %>" class="nav-item"><%= share.tabList[i].name %></li>').insertAfter($t.children().children('ul').children('li').eq(<%= i-1 %>));
+                 $('<div id="<%= share.tabList[i].cid %>"  class="tabContainer" wys-container></div>').insertAfter($t.children(".tabDivList").children('div').eq(<%= i-1 %>)).hide();
+            <% } %>
     }
     else{
         <% if (i==0) { %>
-            $('#<%= share.ids[i] %>').css('width','<%= share.width %>%');
-            $('#<%= share.ids[i] %>').prependTo($t.children('ul'));
+            $('#<%= share.tabList[i].id %>').prependTo($t.children().children('ul'));
         <% } %>
         <% if (i!=0) { %>
-            $('#<%= share.ids[i] %>').css('width','<%= share.width %>%');
-            $('#<%= share.ids[i] %>').insertAfter($t.children('ul').children('li').eq(<%= i-1 %>));
+            $('#<%= share.tabList[i].id %>').insertAfter($t.children().children('ul').children('li').eq(<%= i-1 %>));
         <% } %>
     }
     <% } %>
 </stage-js-wys>
+
 <stage-javascript type="text/javascript">
-
-
+    $($t.children(".wys-nav-List").children(".nav-inner")).on("click","li",function(){
+        $t.children(".wys-nav-List").children(".nav-inner").find(".nav-item").removeClass("cur");
+        $t.children(".tabDivList").find(".tabContainer").hide();
+        $(this).addClass("cur");
+        $("#con-"+$(this).attr("id")).show();
+    });
 </stage-javascript>
+
+
 <stage-css>
-.layout-container-table.edit-page {
-    padding: 5px 1px;
-}
-.layout-container-table {
-    display: -ms-flexbox;
-    display: flex;
-    position: relative;
-    min-height: 50px;
-    width: 100%;
-}
-.layout-container-table .snapshoot-box {
-    -ms-flex: 0 0 auto;
-    flex: 0 0 auto;
-    min-height: 50px;
-    vertical-align: top;
-    box-sizing: border-box;
-    position: relative;
-    background: none;
-    list-style: none;
-    min-height: 40px!important;
-}
-.layout-container-table.edit-page .snapshoot-box~.snapshoot-box:before {
-    content: "";
-    position: absolute;
-    left: 0;
-    top: 0;
-    bottom: 0;
-    width: 1px;
-    background-color: #999;
-}
+    .tabDivList {
+        width:100%;
+        overflow: hidden;
+    }
+    .tabDivList .tabContainer {
+        min-height:200px;
+    }
+    .wys-nav-List {
+        cursor: pointer;
+        position: relative;
+        height: 46px;
+        line-height: 46px;
+        text-align: center;
+        background-color: #fff;
+        width:100%;
+        overflow-x: auto;
+    }
+    .wys-nav-List .nav-inner {
+        margin: 0;
+        padding: 0;
+        height: 46px;
+        display: -webkit-box;
+        -webkit-box-align: center;
+    }
+    .wys-nav-List .nav-item {
+        position: relative;
+        vertical-align: top;
+        display: block;
+        -webkit-box-flex: 1;
+        width: 1px;
+        padding-right: 15px;
+    }
+    .wys-nav-List .line {
+        display: inline-block;
+        vertical-align: middle;
+        height: 14px;
+        line-height: 14px;
+        border-right: 1px solid #222;
+        position: absolute;
+        right: 7px;
+        top: 50%;
+        margin-top: -6px;
+    }
+    .wys-nav-List .nav-item a {
+        color: #222;
+        font-size: 14px;
+        display: block;
+        width: 100%;
+        line-height: 24px;
+        height: 24px;
+        vertical-align: middle;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        white-space: nowrap;
+        box-sizing: border-box;
+    }
+    .navitem0 .cur  {
+        background-color: #f85959;
+        border-radius: 12px;
+        color: #fff;
+    }
+    .navitem1 .cur {
+        line-height: 44px;
+        height: 44px;
+        color: #f85959;
+        border-bottom: 2px solid #f85959;
+    }
+    .navitem2 .cur  {
+        color: #f85959;
+    } 
 </stage-css>
